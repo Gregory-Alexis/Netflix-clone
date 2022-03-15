5d4bfa791f3ad176a751268d21b322275849739c
Westindiess <gregoryalexiss@gmail.com>
2022-03-16 00:10:43 +0100

    My List component

---
+++ src/components/MyList/MyList.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import NavBar from "../HomeComponents/NavBar/NavBar";
+import MyListItem from "./MyListItem";
+
+const MyList = () => {
+  const myList = useSelector((state) => state.myListData.todoList);
+
+  return (
+    <div className="bg-darknet">
+      <NavBar />
+      <h1 className="text-white pl-10 text-4xl pt-24">My List</h1>
+      <div className="h-screen flex flex-wrap pl-10">
+        {myList.map((item) => (
+          <MyListItem
+            id={item.id}
+            key={item.id}
+            poster={item.poster}
+            name={item.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyList;
