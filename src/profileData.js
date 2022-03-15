65e861752e97cba145a4de94395e2fe6e09e8db4
Westindiess <gregoryalexiss@gmail.com>
2022-03-15 23:50:15 +0100

    profile data

---
+++ src/profileData.js
@@ -0,0 +1,16 @@
+const profileData = [
+  {
+    id: 1,
+    link: "Account",
+  },
+  {
+    id: 2,
+    link: "Help Center",
+  },
+  {
+    id: 3,
+    link: "Sign out of Netflix",
+  },
+];
+
+export default profileData;
