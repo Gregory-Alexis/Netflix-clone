2af35666e9236ac4158e5df01cef1ab7f4a0ef45
Westindiess <gregoryalexiss@gmail.com>
2022-03-16 00:33:51 +0100

    tv shows video

---
+++ src/pages/Video/TvShowsVideo.jsx
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+import Loading from "../../components/Loading/Loading";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import axios from "axios";
+import {
+  setError,
+  setLoading,
+  setTvShowsData,
+} from "../../redux/tvShowsSlice/tvShowsSlice";
+
+const TvShowsVideo = () => {
+  const { id } = useParams();
+  const tvShowsData = useSelector((state) => state.tvShowsData.data);
+  const loading = useSelector((state) => state.tvShowsData.loading);
+  const error = useSelector((state) => state.tvShowsData.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setLoading(true));
+
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(
+          `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${process.env.REACT_APP_MOVIE_API}&language=fr-Fr`
+        );
+        dispatch(setTvShowsData(result.data.results.slice(0, 1)));
+      } catch (err) {
+        dispatch(setError(err.message));
+      }
+    };
+
+    fetchData();
+  }, [dispatch, id]);
+
+  return (
+    <>
+      {loading && <Loading />}
+      {error && (
+        <p className="text-white flex justify-center items-center">{error}</p>
+      )}
+      {tvShowsData.length === 0 && (
+        <p className="text-white  flex justify-center items-center bg-darknet h-screen text-4xl px-6 text-center">
+          Désolé, nous n'avons pas encore cette vidéo dans notre catalogue.
+        </p>
+      )}
+      <div className="bg-darknet min-h-screen">
+        {tvShowsData.map((el) => (
+          <div className="bg-darknet min-h-screen text-white">
+            <iframe
+              src={`https://www.youtube.com/embed/${el.key}`}
+              title="video"
+              className="w-full h-screen"
+            ></iframe>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TvShowsVideo;
