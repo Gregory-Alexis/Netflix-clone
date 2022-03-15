0f59206365dd922d659bfd25d016c4b9472736d5
Westindiess <gregoryalexiss@gmail.com>
2022-03-15 23:50:34 +0100

    tv shows id data

---
+++ src/TvShowsIdData.js
@@ -0,0 +1,34 @@
+export const tvShowsId = [
+  {
+    id: 10759,
+    genre: "Action & Adventure",
+  },
+  {
+    id: 16,
+    genre: "Animation",
+  },
+  {
+    id: 35,
+    genre: "Comédie",
+  },
+  {
+    id: 80,
+    genre: "Crime",
+  },
+  {
+    id: 18,
+    genre: "Drama",
+  },
+  {
+    id: 10751,
+    genre: "Family",
+  },
+  {
+    id: 9648,
+    genre: "Mystère",
+  },
+  {
+    id: 10763,
+    genre: "News",
+  },
+];
