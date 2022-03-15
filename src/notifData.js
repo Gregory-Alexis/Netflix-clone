61798b97377b3dcdb4f515635a1a70e786e0979f
Westindiess <gregoryalexiss@gmail.com>
2022-03-15 23:50:11 +0100

    notification data

---
+++ src/notifData.js
@@ -0,0 +1,82 @@
+export const notifData = [
+  {
+    id: 1,
+    title: "New Arrival",
+    subtitle: "The Tinder Swindler",
+    when: "Today",
+    img: "https://dnm.nflximg.net/api/v6/kvDymu0eXRyicIuSUzvRrxrm5dU/AAAABTKOKu9RKm4kdpKdJfhxIx1131YJwPFreYIzwrh4hlkUDg2UCdUNokvM9cXNioZGIFozNq_ZCCIsNtjvlr4YyyvskuBhqYutHk861pwy3EbKlOtpXCOeXsQuE5AYs7rHbqXp7jE.jpg?r=b43",
+  },
+
+  {
+    id: 2,
+    title: "Rewatch your favorite moments",
+    subtitle: "See what you've watch",
+    when: "2 days ago",
+    img: "https://dnm.nflximg.net/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUBhCL-9PUCxIhEdEvSXhu6H7w4aNFVIl8Fx_evhOFBJLgRtcuWdzwT-wu7M3uQy7RdLFZByOc-Zq7u4x50yKBA.jpg?r=5cb",
+  },
+
+  {
+    id: 3,
+    title: "Netflix Lookahead",
+    subtitle: "Explore what's coming soon",
+    when: "4 days ago",
+    img: "https://assets.nflxext.com/us/email/gem/comingsoon/csInapp_112x63.png",
+  },
+
+  {
+    id: 4,
+    title: "New Arrival",
+    subtitle:
+      "The Woman in the House Across the Street from the Girl in the Window",
+    when: "5 days ago",
+    img: "https://dnm.nflximg.net/api/v6/kvDymu0eXRyicIuSUzvRrxrm5dU/AAAABeuAC6MUse2jZmGOqZ6QYzMq5fPZD2fgxEaUqdeyJHL4x9U_OwqPSF-3MgIIgzB7mm3FlFDuh3T2GJgIlWWzfuVyWA_sxYG_RvLE5cfiWkgRn3Nz5LOc_5Z6o2ptjLZsA4Hy7bE.jpg?r=35f",
+  },
+
+  {
+    id: 5,
+    title: "Now Available",
+    subtitle: "Season 3",
+    when: "1 week ago",
+    img: "https://dnm.nflximg.net/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABftneF3DCAKOBHDrJ8d4X18Ogxjlxtg9B2BvMRK5QE9LLLc6WLQ3yFtk5zOscFgYUrYBzd3oX3jN57I9xVq6P40.jpg?r=d1e",
+  },
+
+  {
+    id: 6,
+    title: "New Arrival",
+    subtitle: "Neymar: The Perfect chaos",
+    when: "1 week ago",
+    img: "https://dnm.nflximg.net/api/v6/kvDymu0eXRyicIuSUzvRrxrm5dU/AAAABZW2bI8zGp5ecDAeK7SseLulb27i4AyUSAtRjtzc3ADZC1rB0IRwyMqnY9rm2QAf-32hDj_PEBqOfBu-EDr3kliOlODmF57l2kL6vqvXH8xMO0jibCUXHh-zggg4N_XJ46gu6S0.jpg?r=d68",
+  },
+
+  {
+    id: 7,
+    title: "Top 10 Today",
+    subtitle: "France Watch them all",
+    when: "2 weeks ago",
+    img: "https://dnm.nflximg.net/api/v6/kvDymu0eXRyicIuSUzvRrxrm5dU/AAAABWEugwf8HxXaVdvvvRpNDQhJmxBLNlK49-6Aei1kWdc8FiPEl2qD78B5oeGvpvkd4z8zcZIWNaj-KZFyCW5hxzHMwKARPzwLFNEOW38w9wisM-drrIcOvot2xwQzJqn6CekDFzI.jpg?r=253",
+  },
+
+  {
+    id: 8,
+    title: "New arrival",
+    subtitle: "Archive 81",
+    when: "2 weeks ago",
+    img: "https://dnm.nflximg.net/api/v6/kvDymu0eXRyicIuSUzvRrxrm5dU/AAAABeNKfSBazD2vmJneF8B_d6FhGCq51oqFnZwQOeKexGQO8SHwbe36uoRnFAa8dKveY05LTwqEhwqO-tqiPGXn8eVdNTHzsJUF0M2cNjv53qZeickteSaFZnQ8HW8FxIWU-gx3VY8.jpg?r=7b6",
+  },
+
+  {
+    id: 9,
+    title: "New arrival",
+    subtitle: "Brazen",
+    when: "2 weeks ago",
+    img: "https://dnm.nflximg.net/api/v6/kvDymu0eXRyicIuSUzvRrxrm5dU/AAAABVL0leV6KR91UridIQTFkENemAxnzg9t7QTTV2kup62d7DZHjKMIqcWwdG433QTqKeRJctH8f8x61bvoJHwjrvhGNmHp5XWhR2BaehEoPeVQLvMTG9DiFWqFG_xAq7lGZUaRrXE.jpg?r=0ba",
+  },
+
+  {
+    id: 10,
+    title: "New arrival",
+    subtitle: "The Wasteland",
+    when: "3 weeks ago",
+    img: "https://dnm.nflximg.net/api/v6/kvDymu0eXRyicIuSUzvRrxrm5dU/AAAABZZ6-4IaigQplsBCDfMvijGIFnh_VZmgPVnNMaaYPLBACe0Lc2pw3gF-y3Dm75TNLzpo1YKX1s50ymQBwCY15HHTm2x0HudfxGEtamzZRWtltbnKz34SwUe6IOBcweigtTSfjy0.jpg?r=f5e",
+  },
+];
