d930578ac0c5fe4a9b7b4e8fae1aa6835eba401a
Westindiess <gregoryalexiss@gmail.com>
2022-03-16 23:59:58 +0100

    new navbar

--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
 import NavBar from "../HomeComponents/NavBar/NavBar";
 import MyListItem from "./MyListItem";
+import NavbarMyList from "./NavbarMyList";
 
 const MyList = () => {
   const myList = useSelector((state) => state.myListData.myList);
 
   return (
     <div className="bg-darknet min-h-screen">
-      <NavBar />
+      <NavbarMyList />
       <h1 className="text-white pl-10  pt-24 text-3xl xl:text-4xl">My List</h1>
       <div className="flex pl-10 flex-wrap">
         {myList.map((item) => (
