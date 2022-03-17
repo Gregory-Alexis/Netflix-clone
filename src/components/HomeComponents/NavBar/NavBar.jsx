2603f91e1f1a9ed8f7aad084049436ead5540286
Westindiess <gregoryalexiss@gmail.com>
2022-03-17 21:38:40 +0100

    class to className

--- src/components/HomeComponents/NavBar/NavBar.jsx
+++ src/components/HomeComponents/NavBar/NavBar.jsx
@@ -110,7 +110,7 @@ const NavBar = () => {
               )}
             </ul>
           ) : (
-            <div class="relative inline-block text-center group">
+            <div className="relative inline-block text-center group">
               <div>
                 <button
                   type="button"
@@ -142,7 +142,7 @@ const NavBar = () => {
                 role="menu"
                 aria-orientation="vertical"
                 aria-labelledby="menu-button"
-                tabindex="-1"
+                tabIndex="-1"
               >
                 <div
                   className="py-1 bg-darknet text-gray-400 bg-opacity-95 "
