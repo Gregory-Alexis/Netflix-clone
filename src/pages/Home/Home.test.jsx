58abbf4afc5c0da2849f9b25e9153641c553b88b
Westindiess <gregoryalexiss@gmail.com>
2022-03-20 22:51:22 +0100

    Home test

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -32,41 +32,6 @@ describe("Home page test suits", () => {
     expect(loading).not.toBeInTheDocument();
   });
 
-  it("should display netflix logo", () => {
-    const img = screen.getByAltText("Netflix");
-    expect(img).toBeInTheDocument();
-  });
-
-  it("should display the link", () => {
-    const homeLink = screen.getByText("Home");
-    const tvLink = screen.getByText("TV Shows");
-    const movieLink = screen.getByText("Movies");
-    const napLink = screen.getByText("New and Popular");
-    const myListLink = screen.getByText("My List");
-    const kidsLink = screen.getByText("Kids");
-    expect(homeLink).toBeInTheDocument();
-    expect(tvLink).toBeInTheDocument();
-    expect(movieLink).toBeInTheDocument();
-    expect(napLink).toBeInTheDocument();
-    expect(myListLink).toBeInTheDocument();
-    expect(kidsLink).toBeInTheDocument();
-  });
-
-  it("Should be initialy an empty search input", () => {
-    const input = screen.getByPlaceholderText("Titles, people, genres");
-    expect(input).toBeInTheDocument();
-    fireEvent.click(input);
-    expect(input).toBeEmptyDOMElement();
-  });
-
-  it("Should be able to write in the search input", () => {
-    const input = screen.getByPlaceholderText("Titles, people, genres");
-    expect(input).toBeInTheDocument();
-    fireEvent.click(input);
-    fireEvent.change(input, { target: { value: "drame" } });
-    expect(input.value).toBe("drame");
-  });
-
   it("Should be redirect correctly", async () => {
     const link = screen.getByText("Kids");
     expect(link).toBeInTheDocument();
