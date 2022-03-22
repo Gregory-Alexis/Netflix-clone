2b6e19039f4ff09ba4c3d58ff5c820e3f7fcdd34
Westindiess <gregoryalexiss@gmail.com>
2022-03-22 22:32:43 +0100

    custom featured tv shows data

---
+++ src/components/TvShowsComponents/FeaturedData/FeaturedTvShowsData.jsx
@@ -0,0 +1,7 @@
+import FeaturedDataCustom from "../../CustomComponents/FeaturedDataCustom";
+
+const FeaturedTvShowsData = ({ featuredTvShowsData }) => {
+  return <FeaturedDataCustom props={featuredTvShowsData} link="tv-video" />;
+};
+
+export default FeaturedTvShowsData;
