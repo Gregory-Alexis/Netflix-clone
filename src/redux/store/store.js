c00ca318a467f322edd58129ec69bd210b03c35d
Westindiess <gregoryalexiss@gmail.com>
2022-03-23 00:59:02 +0100

    remove unused slice

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,20 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import homeSlice from "../homeSlice/homeSlice";
-import kidsSlice from "../kidsSlice/kidsSlice";
-import movieSlice from "../movieSlice/movieSlice";
-import suggestionMovieSlice from "../movieSlice/suggestionMovieSlice";
+import appSlice from "../appSlice/appSlice";
 import myListSlice from "../myListSlice/myListSlice";
-import suggestionTvShowsSlice from "../tvShowsSlice/suggestionTvShowsSlice";
-import tvShowsSlice from "../tvShowsSlice/tvShowsSlice";
 
 export const store = configureStore({
   reducer: {
-    homeData: homeSlice,
-    tvShowsData: tvShowsSlice,
-    movieData: movieSlice,
-    suggestionMovieData: suggestionMovieSlice,
-    suggestionTvShowsData: suggestionTvShowsSlice,
+    appData: appSlice,
     myListData: myListSlice,
-    kidsData: kidsSlice,
   },
 });
