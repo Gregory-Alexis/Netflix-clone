88e5f24e93ea1ff55bb294106190744ab6d09feb
Westindiess <gregoryalexiss@gmail.com>
2022-03-22 22:34:39 +0100

    suggestion movie page folder renamed

--- src/pages/Movie/SuggestionMoviePage.jsx
+++ src/pages/Movie/SuggestionMoviePage.jsx
@@ -1,22 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import NavBarCustom from "../../components/CustomComponents/NavbarCustom";
 import Footer from "../../components/Footer/Footer";
 import Loading from "../../components/Loading/Loading";
-import NavBarMovieSuggestion from "../../components/MovieComponents/NavBar/NavBarMovieSuggestion";
+import SubNavBarMovieSuggestion from "../../components/MovieComponents/NavBar/SubNavBarMovieSuggestion";
 import getHomeList from "../../dataHomeFetch";
-import {
-  setError,
-  setLoading,
-  setSuggestionMovieData,
-} from "../../redux/movieSlice/suggestionMovieSlice";
+import { setError, setLoading } from "../../redux/homeSlice/homeSlice";
 import SuggestionMoviePageItem from "./SuggestionMoviePageItem";
 
 const SuggestionMoviePage = () => {
-  const suggestionMovieData = useSelector(
-    (state) => state.suggestionMovieData.suggestionMovieData
-  );
-  const loading = useSelector((state) => state.suggestionMovieData.loading);
-  const error = useSelector((state) => state.suggestionMovieData.error);
+  const [suggestionMovieData, setSuggestionMovieData] = useState([]);
+  const loading = useSelector((state) => state.homeData.loading);
+  const error = useSelector((state) => state.homeData.error);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -25,7 +20,9 @@ const SuggestionMoviePage = () => {
       try {
         const result = await getHomeList();
 
-        dispatch(setSuggestionMovieData(result));
+        setSuggestionMovieData(result);
+
+        dispatch(setLoading(false));
       } catch (err) {
         dispatch(setError(err.message));
       }
@@ -37,8 +34,11 @@ const SuggestionMoviePage = () => {
   return (
     <div className="bg-darknet min-h-screen">
       {loading && <Loading />}
-      {error && <p>{error}</p>}
-      <NavBarMovieSuggestion dispatch={dispatch} />
+      {error && (
+        <p className="text-white flex justify-center items-center">{error}</p>
+      )}
+      <NavBarCustom active="movie" />
+      <SubNavBarMovieSuggestion suggestion="movie-suggestion" />
       <div className="pt-20">
         {suggestionMovieData.map((el) => (
           <SuggestionMoviePageItem
