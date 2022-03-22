766a1c6be2d7d94f6a5d5dc09a446302438d9149
Westindiess <gregoryalexiss@gmail.com>
2022-03-22 22:31:11 +0100

    custom movie error filter page

--- src/components/MovieComponents/FeaturedData/ErrorMovieFilterPage.jsx
+++ src/components/MovieComponents/FeaturedData/ErrorMovieFilterPage.jsx
@@ -1,23 +1,9 @@
-const ErrorFilterMoviePage = ({ search }) => {
-  return (
-    <div className="bg-darknet text-white flex items-center justify-center pt-48">
-      <div>
-        {/*Affichera ce que l'utilisateur à écrit en plus du message d'erreur */}
-        <p className="mb-4">
-          Votre recherche pour "{search}" ne trouve aucune correspondance.
-        </p>
-        <p className="mb-4">Suggestions:</p>
-        <ul className="list-disc pl-10">
-          <li>Essayez différents mots clef</li>
-          <li>Recherchez vous un Film ou une Série ?</li>
-          <li>
-            Essayez avec un film, un titre de série, un acteur ou un réalisateur
-          </li>
-          <li>Essayez un genre comme, comédie, romance, sports, ou drame</li>
-        </ul>
-      </div>
-    </div>
-  );
+import { useSelector } from "react-redux";
+import ErrorFilterPageCustom from "../../CustomComponents/ErrorFilterPageCustom";
+
+const ErrorFilterMoviePage = () => {
+  const search = useSelector((state) => state.homeData.search);
+  return <ErrorFilterPageCustom search={search} />;
 };
 
 export default ErrorFilterMoviePage;
