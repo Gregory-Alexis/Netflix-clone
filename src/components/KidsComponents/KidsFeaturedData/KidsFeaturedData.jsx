7229dedc6910bb94e7628bf12f8842715e7f8f4b
Westindiess <gregoryalexiss@gmail.com>
2022-03-22 22:30:33 +0100

    Custom kids featured data

---
+++ src/components/KidsComponents/KidsFeaturedData/KidsFeaturedData.jsx
@@ -0,0 +1,7 @@
+import FeaturedDataCustom from "../../CustomComponents/FeaturedDataCustom";
+
+const KidsFeaturedData = ({ featuredKidsData }) => {
+  return <FeaturedDataCustom props={featuredKidsData} link="tv-video" />;
+};
+
+export default KidsFeaturedData;
