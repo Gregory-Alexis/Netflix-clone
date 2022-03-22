ced2007172d69470e7817067abb9dda9e52eae39
Westindiess <gregoryalexiss@gmail.com>
2022-03-22 22:30:08 +0100

    custom notif drop down

--- src/components/HomeComponents/NavBar/NotificationDropDown.jsx
+++ src/components/HomeComponents/NavBar/NotificationDropDown.jsx
@@ -1,37 +1,8 @@
-import { ArrowDropUp } from "@mui/icons-material";
-import { notifData } from "../../../notifData";
+import NotificationDropDownCustom from "../../CustomComponents/NotifDropDownCustom";
 
 const NotificationDropDown = () => {
   // Composant qui affiche les notifications utilisateur
-  return (
-    <>
-      <div className="NotificationDropdownContainer">
-        <div
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="menu-button"
-          tabIndex="-1"
-        >
-          <ul className="overflow-auto h-70 cursor-pointer">
-            {notifData.map((el) => (
-              <li className="dropdownLinkNotif" key={el.subtitle}>
-                <img src={el.img} alt="notifications" width="100" />
-                <div className="ml-5 hover:text-white w-full h-full">
-                  <h1>{el.title}</h1>
-                  <p>{el.subtitle}</p>
-                  <p className="text-gray-500 text-xs">{el.when}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      <svg className="ArrowUpNotification">
-        <ArrowDropUp />
-      </svg>
-    </>
-  );
+  return <NotificationDropDownCustom />;
 };
 
 export default NotificationDropDown;
