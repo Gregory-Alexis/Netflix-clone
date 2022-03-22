a22b565cab86b84c0c87d0dd18b2619c87ffd6b1
Westindiess <gregoryalexiss@gmail.com>
2022-03-22 22:35:20 +0100

    tv details custom navbar added

--- src/pages/TvDetails/TvDetails.jsx
+++ src/pages/TvDetails/TvDetails.jsx
@@ -4,9 +4,9 @@ import { useParams } from "react-router";
 import axios from "axios";
 import Loading from "../../components/Loading/Loading";
 import { useEffect, useState } from "react";
-import NavBar from "../../components/HomeComponents/NavBar/NavBar";
 import { Link } from "react-router-dom";
 import { addToList } from "../../redux/myListSlice/myListSlice";
+import NavBarCustom from "../../components/CustomComponents/NavbarCustom";
 
 const TvDetails = () => {
   const { id } = useParams();
@@ -36,52 +36,59 @@ const TvDetails = () => {
   }, [dispatch, id]);
   return (
     <>
-      {loading && <Loading />}
-      {error && (
-        <p className="text-white flex justify-center items-center">{error}</p>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {error && (
+            <p className="text-white flex justify-center items-center">
+              {error}
+            </p>
+          )}
+          <NavBarCustom />
+          <div className="bg-darknet text-gray-100 min-h-screen flex flex-col items-center pt-24 px-5 xl:pl-24">
+            <div>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${tvDetails.poster_path}`}
+                alt={tvDetails.poster_path}
+                key={tvDetails.id}
+              />
+            </div>
+            <div className="flex justify-center w-full">
+              <h1 className=" text-2xl pt-5">{tvDetails.title}</h1>
+            </div>
+            <div className="flex flex-col items-center w-full">
+              <h2 className="text-center mt-5 text-lg">Synopsis:</h2>
+              <p className="py-2 leading-7 lg:w-3/4 xl:w-2/4">
+                {tvDetails.overview}
+              </p>
+            </div>
+            <Link to={`/tv-video/${id}`} className="flex justify-center">
+              <button
+                type="button"
+                aria-label="link button"
+                className="bg-blue-700 px-12 py-2 mb-2 rounded-md text-lg hover:bg-blue-800"
+              >
+                Watch Trailer
+              </button>
+            </Link>
+            <button
+              className="border hover:bg-gray-500 text-gray-100 px-11 py-2 mb-2 rounded-md text-lg"
+              onClick={() =>
+                dispatch(
+                  addToList({
+                    poster: tvDetails.poster_path,
+                    name: tvDetails.original_name,
+                    id: tvDetails.id,
+                  })
+                )
+              }
+            >
+              Add to my List
+            </button>
+          </div>
+        </>
       )}
-      <NavBar />
-      <div className="bg-darknet text-gray-100 min-h-screen flex flex-col items-center pt-24 px-5 xl:pl-24">
-        <div>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${tvDetails.poster_path}`}
-            alt={tvDetails.poster_path}
-            key={tvDetails.id}
-          />
-        </div>
-        <div className="flex justify-center w-full">
-          <h1 className=" text-2xl pt-5">{tvDetails.title}</h1>
-        </div>
-        <div className="flex flex-col items-center w-full">
-          <h2 className="text-center mt-5 text-lg">Synopsis:</h2>
-          <p className="py-2 leading-7 lg:w-3/4 xl:w-2/4">
-            {tvDetails.overview}
-          </p>
-        </div>
-        <Link to={`/tv-video/${id}`} className="flex justify-center">
-          <button
-            type="button"
-            aria-label="link button"
-            className="bg-blue-700 px-12 py-2 mb-2 rounded-md text-lg hover:bg-blue-800"
-          >
-            Watch Trailer
-          </button>
-        </Link>
-        <button
-          className="border hover:bg-gray-500 text-gray-100 px-11 py-2 mb-2 rounded-md text-lg"
-          onClick={() =>
-            dispatch(
-              addToList({
-                poster: tvDetails.poster_path,
-                name: tvDetails.original_name,
-                id: tvDetails.id,
-              })
-            )
-          }
-        >
-          Add to my List
-        </button>
-      </div>
     </>
   );
 };
