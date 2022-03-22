5fe7f98ccf91092e9440c1ec80ccb9cb7cc1c399
Westindiess <gregoryalexiss@gmail.com>
2022-03-22 22:34:53 +0100

    add custom navbar

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -4,9 +4,9 @@ import { useParams } from "react-router";
 import axios from "axios";
 import Loading from "../../components/Loading/Loading";
 import { useEffect, useState } from "react";
-import NavBar from "../../components/HomeComponents/NavBar/NavBar";
 import { Link } from "react-router-dom";
 import { addToList } from "../../redux/myListSlice/myListSlice";
+import NavBarCustom from "../../components/CustomComponents/NavbarCustom";
 
 const MovieDetails = () => {
   const { id } = useParams();
@@ -37,52 +37,57 @@ const MovieDetails = () => {
 
   return (
     <>
-      {loading && <Loading />}
       {error && (
         <p className="text-white flex justify-center items-center">{error}</p>
       )}
-      <NavBar />
-      <div className="bg-darknet text-gray-100 min-h-screen flex flex-col items-center pt-24 px-5 xl:pl-24">
-        <div>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
-            alt={movieDetails.poster_path}
-            key={movieDetails.id}
-          />
-        </div>
-        <div className="flex justify-center w-full">
-          <h1 className=" text-2xl pt-5">{movieDetails.title}</h1>
-        </div>
-        <div className="flex flex-col items-center w-full">
-          <h2 className="text-center mt-5 text-lg">Synopsis:</h2>
-          <p className="py-2 leading-7 lg:w-3/4 xl:w-2/4">
-            {movieDetails.overview}
-          </p>
-        </div>
-        <Link to={`/movie-video/${id}`} className="flex justify-center">
-          <button
-            type="button"
-            aria-label="link button"
-            className="bg-blue-700 px-12 py-2 mb-2 rounded-md text-lg hover:bg-blue-800"
-          >
-            Watch Trailer
-          </button>
-        </Link>
-        <button
-          className="border hover:bg-gray-500 text-gray-100 px-11 py-2 mb-2 rounded-md text-lg"
-          onClick={() =>
-            dispatch(
-              addToList({
-                poster: movieDetails.poster_path,
-                name: movieDetails.original_name,
-                id: movieDetails.id,
-              })
-            )
-          }
-        >
-          Add to my List
-        </button>
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <NavBarCustom />
+          <div className="bg-darknet text-gray-100 min-h-screen flex flex-col items-center pt-24 px-5 xl:pl-24">
+            <div>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+                alt={movieDetails.poster_path}
+                key={movieDetails.id}
+              />
+            </div>
+            <div className="flex justify-center w-full">
+              <h1 className=" text-2xl pt-5">{movieDetails.title}</h1>
+            </div>
+            <div className="flex flex-col items-center w-full">
+              <h2 className="text-center mt-5 text-lg">Synopsis:</h2>
+              <p className="py-2 leading-7 lg:w-3/4 xl:w-2/4">
+                {movieDetails.overview}
+              </p>
+            </div>
+            <Link to={`/movie-video/${id}`} className="flex justify-center">
+              <button
+                type="button"
+                aria-label="link button"
+                className="bg-blue-700 px-12 py-2 mb-2 rounded-md text-lg hover:bg-blue-800"
+              >
+                Watch Trailer
+              </button>
+            </Link>
+            <button
+              className="border hover:bg-gray-500 text-gray-100 px-11 py-2 mb-2 rounded-md text-lg"
+              onClick={() =>
+                dispatch(
+                  addToList({
+                    poster: movieDetails.poster_path,
+                    name: movieDetails.original_name,
+                    id: movieDetails.id,
+                  })
+                )
+              }
+            >
+              Add to my List
+            </button>
+          </div>
+        </>
+      )}
     </>
   );
 };
