a019575e19488105e9ea7c55d81ba5f1c6b40d79
Westindiess <gregoryalexiss@gmail.com>
2022-03-23 12:14:40 +0100

    change selector reducer

--- src/components/KidsComponents/KidsFeaturedData/ErrorFilterTvKidsPage.jsx
+++ src/components/KidsComponents/KidsFeaturedData/ErrorFilterTvKidsPage.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import ErrorFilterPageCustom from "../../CustomComponents/ErrorFilterPageCustom";
 
 const ErrorFilterTvKidsPage = () => {
-  const search = useSelector((state) => state.kidsData.search);
+  const search = useSelector((state) => state.appData.search);
   return <ErrorFilterPageCustom search={search} />;
 };
 
